4376d218ac2ed48f0f906092b6c51406a4296104
Danil Pismenny <danil@brandymint.ru>
2016-05-19 13:49:21 +0300

    remigration add serach account by title

--- lib/openbill/migration.rb
+++ lib/openbill/migration.rb
@@ -1,5 +1,15 @@
 module Openbill::Migration
   def up
+    openbill_up
+  end
+
+  def down
+    openbill_down
+  end
+
+  private
+
+  def openbill_up
     Dir.entries(sql_dir).select{|f| File.file? sql_dir + f }.sort.each do |file|
       say_with_time "Migrate with #{file}" do
         execute File.read sql_dir + file
@@ -7,14 +17,17 @@ module Openbill::Migration
     end
   end
 
-  def down
+  def openbill_down
     execute "DROP TABLE IF EXISTS OPENBILL_ACCOUNTS CASCADE"
     execute "DROP TABLE IF EXISTS OPENBILL_TRANSACTIONS CASCADE"
     execute "DROP TABLE IF EXISTS OPENBILL_GOODS CASCADE"
     execute "DROP TABLE IF EXISTS OPENBILL_GOODS_AVAILABILITIES CASCADE"
   end
 
-  private
+  def openbill_reset
+    down
+    up
+  end
 
   def sql_dir
     Openbill.root + '/sql/'
