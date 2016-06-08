d093700f7e37dd2d23c84f3691f33d08fbfcf9c9
Danil Pismenny <danil@brandymint.ru>
2016-06-08 10:59:59 +0300

    configure max_connections

--- lib/openbill/configuration.rb
+++ lib/openbill/configuration.rb
@@ -2,12 +2,21 @@ require 'singleton'
 
 class Configuration
   DEFAULT_CURRENCY = 'USD'.freeze
+  MAX_CONNECTIONS = 10
 
   include Singleton
 
   attr_accessor :database
 
-  attr_writer :default_currency
+  attr_writer :logger, :default_currency, :max_connections
+
+  def logger
+    @logger || Rails.logger
+  end
+
+  def max_connections
+    @max_connections || MAX_CONNECTIONS
+  end
 
   def default_currency
     @default_currency || DEFAULT_CURRENCY
--- lib/openbill/database.rb
+++ lib/openbill/database.rb
@@ -1,15 +1,12 @@
 module Openbill
   class Database
-    MAX_CONNECTIONS         = 10
-
     def initialize(config)
-      @db = Sequel.connect config, logger: logger, max_connections: MAX_CONNECTIONS
+      fail "Must be a Openbill::Configuration" unless config.is_a? Openbill::Configuration
+      @db = Sequel.connect config.database, logger: config.logger, max_connections: config.max_connections
       @db.extension :pagination
       @db.extension :pg_hstore
     end
 
     attr_reader :db
-
-    delegate :logger, to: Rails
   end
 end
