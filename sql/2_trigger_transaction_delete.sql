9bddb945e02842ca8028c89eecce5b7eb9b6cd4e
Danil Pismenny <danil@brandymint.ru>
2016-07-28 11:13:19 +0300

    fix sql

--- sql/2_trigger_transaction_delete.sql
+++ sql/2_trigger_transaction_delete.sql
@@ -1,6 +1,5 @@
 CREATE OR REPLACE FUNCTION openbill_transaction_delete() RETURNS TRIGGER AS $process_transaction$
 BEGIN
-  -- установить last_transaction_id, counts и _at
   UPDATE OPENBILL_ACCOUNTS SET amount_cents = amount_cents - OLD.amount_cents, transactions_count = transactions_count - 1 WHERE id = OLD.to_account_id;
   UPDATE OPENBILL_ACCOUNTS SET amount_cents = amount_cents + OLD.amount_cents, transactions_count = transactions_count - 1 WHERE id = OLD.from_account_id;
 
