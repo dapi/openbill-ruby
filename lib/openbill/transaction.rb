46e3bf6fa77724a7029cd4efcf27ce4c85df698e
Andrei Sidorov <vozdelaluz@gmail.com>
2016-08-01 16:04:19 +0300

    Add pending webhooks transactions query

--- lib/openbill/transaction.rb
+++ lib/openbill/transaction.rb
@@ -1,5 +1,7 @@
 module Openbill
   class Transaction < Sequel::Model(TRANSACTIONS_TABLE_NAME)
+    NOTIFIED_MESSAGE = 'Notified'
+
     many_to_one :from_account, class: 'Openbill::Account'
     many_to_one :to_account, class: 'Openbill::Account'
     many_to_one :good, class: 'Openbill::Good'
@@ -10,6 +12,26 @@ module Openbill
 
     # Original transaction
     one_to_one :reverse_transaction, class: 'Openbill::Transaction', primary_key: :reverse_transaction_id, key: :id
+
+    def self.with_pending_webhooks
+      dataset
+        .select_all(:t0).distinct(:t0__id).from(Sequel.as(:openbill_transactions, :t0))
+        .left_join(
+          Sequel.as(:openbill_webhook_logs, :t1),
+          t0__id: :t1__transaction_id
+        )
+        .left_join(
+          Sequel.as(:openbill_webhook_logs, :t2),
+          [
+            t1__transaction_id: :t2__transaction_id,
+            t1__url: :t2__url,
+            t2__message: NOTIFIED_MESSAGE
+          ]
+        )
+        .group(:t0__id, :t1__url)
+        .having('COUNT(t2.*) = 0')
+        .order(:t0__id, Sequel.desc(:t0__created_at))
+    end
 
     def <=> (other)
       id <=> other.id
